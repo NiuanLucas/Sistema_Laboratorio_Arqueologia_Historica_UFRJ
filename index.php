7a82364f6b507c90bdadeaa152d1cede333e7662
Niuan Lucas <niuanlucasnicolau@gmail.com>
2021-12-03 01:41:39 -0300

    Teste Config

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
-<script>
-window.location.href = "website/home/inicio.php?pg_id=2";
-</script>
-
-<?php 
-//header('Location: /website/home/index.php');
+<script>
+
+window.location.href = "website/home/inicio.php?pg_id=2";
+
+</script>
+
+
+
+
+
+<?php 
+
+//header('Location: /website/home/index.php');
+// Teste Niuan Github
 ?>
